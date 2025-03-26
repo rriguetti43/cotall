8eb7b9f5a6f09be48f07a8883ba539f6d64ddfdb
roger ramirez <rogier.riguetti@gmail.com>
2025-03-26 00:39:53 -0500

    conexion se cierra pronto

--- config/db.js
+++ config/db.js
@@ -6,6 +6,9 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 db.connect((err) => {
